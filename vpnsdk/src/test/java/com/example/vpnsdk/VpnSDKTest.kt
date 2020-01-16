9a8bfc881f7624e5ffe9694de44b7327e0ca4dca
Yousuf Sohail <myousufsohail@gmail.com>
2020-01-16 23:02:48 +0500

    Add test skeleton

---
+++ vpnsdk/src/test/java/com/example/vpnsdk/VpnSDKTest.kt
@@ -0,0 +1,34 @@
+package com.example.vpnsdk
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class VpnSDKTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun connect() {
+    }
+
+    @Test
+    fun disconnect() {
+    }
+
+    @Test
+    fun vpnConnectionChangedBroadcast() {
+    }
+
+    @Test
+    fun getContext() {
+    }
+}
